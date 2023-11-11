07c1441268ba4246cf6acf0c7b15211af76061bc
Riemann <antonio@Riemanns-MBP.home>
2023-11-11 19:52:31 +0100

    Updated Antora's model

--- Antora_model.py
+++ Antora_model.py
@@ -19,6 +19,18 @@ from functions import *
 warnings.filterwarnings('ignore')
 matplotlib_inline.backend_inline.set_matplotlib_formats('svg', 'pdf')
 
+#%% DEFAULTS
+
+plt.rcParams['text.usetex'] = True
+plt.rcParams['font.family'] = 'Times'
+plt.rcParams['legend.fontsize'] = 12
+plt.rcParams['axes.labelsize'] = 16
+plt.rcParams['axes.titlesize'] = 16
+plt.rcParams['figure.titlesize'] = 16
+plt.rcParams['xtick.labelsize'] = 12
+plt.rcParams['ytick.labelsize'] = 12
+plt.rcParams['lines.markersize'] = 3
+
 
 #%% Graphite Cp
 def cpG(T):
@@ -51,7 +63,7 @@ mDot = 0.04
 rhos = 1800
 ks = 120
 
-Tin = 800 + 273
+Tin = 51 + 273
 
 Tf = np.zeros((Nt, Nx+1))
 Ts = np.zeros((Nt, Nx+1))
@@ -61,10 +73,60 @@ Tf[:,0] = Tin
 bc1 = np.zeros(Nx)
 
 # ICs
-T00 = 300
+T00 = 800 + 273
 Tf[0,:] = T00
 Ts[0,:] = T00
 
+
+#%% MATRIX ASSEMBLY
+
+rhof = 5*101325/(287*T00)
+
+alpha = eps*rhof*cp(T00)
+beta = eps*kair(T00)
+gamma = (1-eps)*rhos*cpG(T00)
+betas = ks*(1 - eps)
+
+u = mDot/(rhof*eps*np.pi*L)
+
+Rep = rhof*d*u/mu(T00)
+Pr = cp(T00)*mu(T00)/kair(T00)
+Nu = 0.664*Rep**0.5*Pr**0.5
+a = Nu*kair(T00)/d
+#h = 6*(1-eps)*beta*(2 + 1.1*Rep**(0.6)*Pr**(1/3))/(d**2)
+
+h = a*6*(1-eps)/d
+
+p = dt*u/dx
+q = 2*beta/alpha*0.5*dt/(dx**2)
+r = h*dt/alpha
+
+rs = dt*h/gamma
+qs = 2*betas*0.5*dt/(gamma*dx**2)
+
+
+
+# Fluid
+diagonals = [np.ones(Nx)*(1 + 2*q + r + p), np.ones(Nx)*(-q), np.ones(Nx)*(-p-q)]
+offsets = [0, 1, -1]
+A = sp.sparse.diags(diagonals, offsets).toarray()
+A[-1,-2] = -2*q - p
+
+A = sp.sparse.csr_matrix(A)
+
+
+# Solid
+diagonals = [np.ones(Nx + 1)*(1 + 2*qs + rs), np.ones(Nx + 1)*(-qs), np.ones(Nx + 1)*(-qs)]
+offsets = [0, 1, -1]
+As = sp.sparse.diags(diagonals, offsets).toarray()
+As[-1,-2] = -2*qs
+As[0,1] = -2*qs
+
+As = sp.sparse.csr_matrix(As)
+
+
+
+#%% SOLUTION
 
 for i in range(1,len(t)):
     
@@ -77,18 +139,21 @@ for i in range(1,len(t)):
     
     # Thermocline TANK -- fluid properties changing with temperature
     
-    alpha = eps*rhoA(Tf[i-1,:])*cp(Tf[i-1,:])
-    beta = eps*kair(Tf[i-1,:])
+    # Air density at 5 bar
+    rhof = 5*101325/(287*T00)
     
-    gamma = (1-eps)*rhos*cpG(Tf[i-1,:])
+    alpha = eps*rhof*cp(T00)
+    beta = eps*kair(T00)
+    
+    gamma = (1-eps)*rhos*cpG(T00)
     betas = ks*(1 - eps)
     
-    u = mDot/(rhoA(Tf[i-1,:])*eps*np.pi*L**2)
+    u = mDot/(rhof*eps*np.pi*L**2)
 
-    Rep = rhoA(Tf[i-1,:])*d*u/mu(Tf[i-1,:])
-    Pr = cp(Tf[i-1,:])*mu(Tf[i-1,:])/kair(Tf[i-1,:])
+    Rep = rhof*d*u/mu(T00)
+    Pr = cp(T00)*mu(T00)/kair(T00)
     Nu = 0.664*Rep**0.5*Pr**0.5
-    a = Nu*kair(Tf[i-1,:])/d
+    a = Nu*kair(T00)/d
     h = 6*(1-eps)*beta*(2 + 1.1*Rep**(0.6)*Pr**(1/3))/(d**2)
 
     p = dt*u/dx
@@ -115,7 +180,8 @@ for i in range(1,len(t)):
         
     # PDE solution -- TANK
     
-    bc1[0] = Tf[i-1,0]*T0
+    bc1[0] = Tin #Tf[i-1,0]
+    
 
     Ts[i,:] = sp.sparse.linalg.spsolve(As, Ts[i-1,:] + rs*Tf[i-1,:])
     Tf[i,1:] = sp.sparse.linalg.spsolve(A, Tf[i-1,1:] + (q + p)*bc1 + \
@@ -123,6 +189,27 @@ for i in range(1,len(t)):
     
     
     # Heat transfer accross molten salts
-    qf[i] = 0.25*u*np.pi*d**2*eps*rhof*(Tf[i,15] - Tf[i,-15])*cpf
+    # qf[i] = 0.25*u*np.pi*d**2*eps*rhof*(Tf[i,15] - Tf[i,-15])*cpf
     
     # theta6[i,k] = Tf[i,-1]/T0
+    
+    
+    
+#%% PLOTS
+
+fig, ax = plt.subplots()
+
+cs1 = ax.contourf(x, t, Tf - 273)
+plt.tight_layout()
+cbar1 = plt.colorbar(cs1)
+cbar1.set_label(r'$T_f$ ($^\circ C$)', fontsize=16)
+ax.set_ylabel(r'$t$ (h)')
+ax.set_xlabel(r'$H$ (m)')
+ax.set_box_aspect(1)
+
+plt.show()   
+    
+    
+    
+    
+    
